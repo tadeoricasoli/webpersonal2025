1176703ff829302b5c4cb95b1f037a9af539f119
Tadeo Pedro Ricasoli <ricasolit@gmail.com>
2025-07-29 22:44:00 -0300

    Add files via upload

--- tadeoricasoli/index.php
+++ tadeoricasoli/index.php
@@ -50,7 +50,7 @@
         <nav>
           <ul>
             <li>
-              <a href="#footer" class="icon solid fa-info-circle">Send me</a>
+              <a href="#footer" class="icon solid fa-info-circle">About</a>
             </li>
           </ul>
         </nav>
